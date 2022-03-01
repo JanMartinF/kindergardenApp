39780571ef4f9ee11a6d326db99c0dc082c4bd95
chrisgith-b <95165904+chrisgith-b@users.noreply.github.com>
2022-03-01 14:36:11 +0100

    cool

--- kindergarten_backend/app.js
+++ kindergarten_backend/app.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 app.listen(port, () => {
