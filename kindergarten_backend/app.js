8d665240ede07eb42d6c11d980af9a89d190d809
Jan Martin <39485249+JanMartinF@users.noreply.github.com>
2022-03-02 15:58:39 +0100

    nothing exciting, end of day push ...

--- kindergarten_backend/app.js
+++ kindergarten_backend/app.js
@@ -3,14 +3,30 @@ const credentials = require('./dbconfig')
 const app = express();
 const port = 3000;
 const path = require('path');
+const { json } = require('express');
+
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
+  let query = req.query()
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+app.get('/dbTest', function(req, res){
+  let query = req.query;
+  console.log(query);
+  res.sendFile(path.join(__dirname, 'build', 'dbTest.html'));
+})
+
+app.get('/getData', function(req, res){
+  let query = req.query
+  let sampleJson = {test: `${query.name}`}
+  let sampleString = 'testString 1'
+  console.log(query)
+  res.send(sampleJson)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  console.log(credentials.user)
 })
